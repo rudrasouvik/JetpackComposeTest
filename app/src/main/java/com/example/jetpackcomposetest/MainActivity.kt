6101e36e3c94773926bbc3f2cf02e96cd981218d
Webgrity <mailsouvik@webgrity.net>
2021-06-16 17:32:15 +0530

    16-06-2021

---
+++ app/src/main/java/com/example/jetpackcomposetest/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.jetpackcomposetest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpackcomposetest.ui.theme.JetpackComposeTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(text = "Hello")
+                Text(text = "World")
+            }
+        }
+    }
+}
