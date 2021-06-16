086cc6ef30f61d130debdc6c202c529e3bba343c
Webgrity <mailsouvik@webgrity.net>
2021-06-16 19:17:08 +0530

    16-06-2021

--- app/src/main/java/com/example/jetpackcomposetest/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetest/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.jetpackcomposetest
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -12,15 +18,59 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposetest.ui.theme.JetpackComposeTestTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val painter = painterResource(id = R.drawable.image1)
+            val description = "Kermit playing in the snow"
+            val title = "Kermit is playing in the snow"
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(16.dp)
+            ) {
+                ImageCard(painter = painter, contentDescription = description, title = title)
+            }
+        }
+    }
+}
 
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription:String,
+    title:String,
+    modifier: Modifier = Modifier
+){
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box(modifier = Modifier.height(200.dp)){
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ){
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
         }
     }
 }
@@ -52,3 +102,22 @@ class MainActivity : ComponentActivity() {
     Text(text = "World")
     Text(text = "Hello")
 }*/
+/*Column(
+                modifier = Modifier
+                    .background(Color.Green)
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth()
+                    .border(5.dp, Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(5.dp)
+                    .border(10.dp, Color.Red)
+                    .padding(10.dp)
+            ) {
+                //Text(text = "Hello", modifier = Modifier.offset(0.dp, 20.dp))
+                Text(text = "Hello", modifier = Modifier.clickable {
+                    Toast.makeText(this@MainActivity, "clicked", Toast.LENGTH_SHORT).show()
+                })
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(text = "World")
+            }*/
