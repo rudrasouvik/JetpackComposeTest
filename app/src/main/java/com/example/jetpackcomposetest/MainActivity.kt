1cd85ebce128378e56c562a55e4bb015dc6e532d
Webgrity <mailsouvik@webgrity.net>
2021-06-16 18:05:16 +0530

    16-06-2021

--- app/src/main/java/com/example/jetpackcomposetest/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetest/MainActivity.kt
@@ -3,23 +3,52 @@ package com.example.jetpackcomposetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposetest.ui.theme.JetpackComposeTestTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(text = "Hello")
-                Text(text = "World")
-            }
+
         }
     }
 }
+
+
+/*Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Green),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            )
+            {
+                Text(text = "Hello")
+                Text(text = "World")
+                Text(text = "Hello")
+            }*/
+
+/*Row(
+    modifier = Modifier
+        .width(200.dp)
+        .fillMaxHeight(0.5f)
+        .background(Color.Green),
+    horizontalArrangement = Arrangement.SpaceEvenly,
+    verticalAlignment = Alignment.CenterVertically
+)
+{
+    Text(text = "Hello")
+    Text(text = "World")
+    Text(text = "Hello")
+}*/
